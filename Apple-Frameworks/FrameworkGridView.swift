cb647a914e73919f0974e6715dc8c3260f223759
ZinThantWin <zinthantwin1997@gmail.com>
2024-06-11 11:49:20 +0630

    rename and rebuilt

---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct FrameworkGridView : View {
+    var body : some View {
+        VStack{
+            Text("Hello Swift!")
+                .font(.system(.largeTitle))
+        }
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
